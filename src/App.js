8023ffb352a21ee01e9d9958cb7a2bd4c246ee09
Eliran Hasin <ehasin@medallia.com>
2020-07-10 21:57:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReportForm from  './pages/report-form/ReportForm';
+
+function App() {
+  return (
+<ReportForm/>
+  );
+}
+
+export default App;
---
+++ src/pages/report-form/ReportForm.js
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+import {Container} from './ReportForm.styled';
+export default function ReportForm(){
+
+  return(
+    <Container>
+      reports
+    </Container>
+  )
+
+}
